43945191075158b2875815bce9c69cf2956f5a60
ImSeDanny <daniel@koenigl-bayern.de>
2020-09-05 18:10:55 +0200

    [DK] - [BF-4] - added formfields

---
+++ src/app/models/contact-data.model.ts
@@ -0,0 +1,13 @@
+export class ContactData {
+  salutation: string;
+  firstName: string;
+  surname: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+
+/*export enum Salutations {
+  MR,
+  MISS =
+}*/
--- src/app/pages/info/contact/contact.component.ts
+++ src/app/pages/info/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {HeaderService} from "../../../services/header/header.service";
+import {ContactData} from "../../../models/contact-data.model";
 
 @Component({
   selector: 'app-contact',
@@ -9,12 +10,19 @@ import {HeaderService} from "../../../services/header/header.service";
 })
 export class ContactComponent implements OnInit {
 
+  contactData = new ContactData();
+
   constructor(private headerService: HeaderService) {
   }
 
   ngOnInit(): void {
     this.headerService.darkToolbar(true);
     this.headerService.foldHeader(true);
+  }
+
+  send(): void {
+    // check mandatory input fields
+    // create e-mail and send it
   }
 
 }
